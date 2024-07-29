316736487eb6ca8357162ad92d9db303701233c2
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-07-29 12:18:49 +0300

    Add mandatory and advanced tasks

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,19 @@
+export default function updateStudentGradeByCity(arr, city, newGrades) {
+  return arr
+    .filter((obj) => obj.location === city)
+    .map((student) => {
+      newGrades.map((studentGrade) => {
+        if (studentGrade.studentId === student.id) {
+          // eslint-disable-next-line no-param-reassign
+          student.grade = studentGrade.grade;
+        }
+
+        if (!student.hasOwnProperty("grade")) {
+          student.grade = "N/A";
+        }
+        return student;
+      });
+
+      return student;
+    });
+}
---
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,10 @@
+export default function hasValuesFromArray(set, array) {
+  let result = true;
+
+  array.map((item) => {
+    if (!set.has(item)) {
+      result = false;
+    }
+  });
+  return result;
+}
