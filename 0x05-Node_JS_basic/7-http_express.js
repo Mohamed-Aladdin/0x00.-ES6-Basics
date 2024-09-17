33c37002b3608750a6ff8910bd74b29d80b9ad65
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-09-17 16:54:08 +0300

    Fix fn

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,8 +1,46 @@
 const express = require('express');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs').promises;
 
 const PORT = 1245;
 const app = express();
+
+function countStudents(path) {
+  return fs
+    .readFile(path, 'utf-8')
+    .then((data) => {
+      const lines = data.trim().toString().split('\n');
+      lines.shift();
+      const studentsByField = {};
+      let totalStudents = 0;
+
+      lines.forEach((line) => {
+        if (line.trim()) {
+          totalStudents += 1;
+          const [firstname, , , field] = line.split(',');
+
+          if (!studentsByField[field]) {
+            studentsByField[field] = [];
+          }
+          studentsByField[field].push(firstname);
+        }
+      });
+
+      let output = `Number of students: ${totalStudents}\n`;
+
+      for (const field in studentsByField) {
+        if (Object.prototype.hasOwnProperty.call(studentsByField, field)) {
+          output += `Number of students in ${field}: ${
+            studentsByField[field].length
+          }. List: ${studentsByField[field].join(', ')}\n`;
+        }
+      }
+
+      return output.trim();
+    })
+    .catch(() => {
+      throw new Error('Cannot load the database');
+    });
+}
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
@@ -10,9 +48,8 @@ app.get('/', (req, res) => {
 
 app.get('/students', (req, res) => {
   countStudents(process.argv[2].toString())
-    .then((op) => {
+    .then((output) => {
       res.write('This is the list of our students\n');
-      const output = op.slice(0, -1);
       res.send(output);
     })
     .catch(() => {
