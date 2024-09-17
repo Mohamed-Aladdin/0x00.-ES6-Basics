fafa980a8d28b440e435b600dd3d5ff15d0eceeb
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-09-17 14:43:31 +0300

    Fix import statement

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-function countStudents(path) {
+function countStudents (path) {
   try {
     const data = fs.readFileSync(path, 'utf-8').trim();
     const lines = data.toString().split('\n');
