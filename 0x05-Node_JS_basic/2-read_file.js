788cac27cc32fbe8a68a7d6e8a9599ad8d2f3fe1
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-09-17 15:13:17 +0300

    Fix fn

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,41 +1,33 @@
 const fs = require('fs');
 
-function countStudents(fileName) {
-  const students = {};
-  const fields = {};
-  let length = 0;
+function countStudents(path) {
   try {
-    const content = fs.readFileSync(fileName, 'utf-8');
-    const lines = content.toString().split('\n');
-    for (let i = 0; i < lines.length; i += 1) {
-      if (lines[i]) {
-        length += 1;
-        const field = lines[i].toString().split(',');
-        if (Object.prototype.hasOwnProperty.call(students, field[3])) {
-          students[field[3]].push(field[0]);
-        } else {
-          students[field[3]] = [field[0]];
+    const data = fs.readFileSync(path, 'utf-8').trim();
+    const lines = data.toString().split('\n');
+    lines.shift();
+    const studentsByField = {};
+
+    if (lines) {
+      lines.forEach((line) => {
+        const [firstname, , , field] = line.split(',');
+
+        if (!studentsByField[field]) {
+          studentsByField[field] = [];
         }
-        if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
-          fields[field[3]] += 1;
-        } else {
-          fields[field[3]] = 1;
-        }
-      }
+        studentsByField[field].push(firstname);
+      });
+      console.log(`Number of students: ${lines.length}`);
     }
-    const l = length - 1;
-    console.log(`Number of students: ${l}`);
-    for (const [key, value] of Object.entries(fields)) {
-      if (key !== 'field') {
-        console.log(
-          `Number of students in ${key}: ${value}. List: ${students[key].join(
-            ', '
-          )}`
-        );
-      }
+
+    for (const field in studentsByField) {
+      console.log(
+        `Number of students in ${field}: ${
+          studentsByField[field].length
+        }. List: ${studentsByField[field].join(', ')}`
+      );
     }
   } catch (error) {
-    throw Error('Cannot load the database');
+    throw new Error('Cannot load the database');
   }
 }
 
