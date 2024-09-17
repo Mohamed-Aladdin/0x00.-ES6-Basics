677774bceb1508162594974a67b99ed96b5bf649
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-09-17 16:38:46 +0300

    Fix fn

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,8 +1,45 @@
 const http = require('http');
-const countStudents = require('./3-read_file_async');
 
 const HOST = '127.0.0.1';
 const PORT = 1245;
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf-8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      const lines = data.trim().split('\n');
+      lines.shift(); // Remove the header
+      const studentsByField = {};
+
+      lines.forEach((line) => {
+        if (line) {
+          const [firstname, , , field] = line.split(',');
+          if (!studentsByField[field]) {
+            studentsByField[field] = [];
+          }
+          studentsByField[field].push(firstname);
+        }
+      });
+
+      const totalStudents = lines.length;
+      let output = `Number of students: ${totalStudents}\n`;
+
+      for (const field in studentsByField) {
+        if (Object.prototype.hasOwnProperty.call(studentsByField, field)) {
+          output += `Number of students in ${field}: ${
+            studentsByField[field].length
+          }. List: ${studentsByField[field].join(', ')}\n`;
+        }
+      }
+
+      resolve(output.trim());
+    });
+  });
+}
 
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -16,7 +53,7 @@ const app = http.createServer((req, res) => {
     res.write('This is the list of our students\n');
     countStudents(process.argv[2].toString())
       .then((output) => {
-        res.end(output.trim());
+        res.end(output);
       })
       .catch(() => {
         res.statusCode = 404;
