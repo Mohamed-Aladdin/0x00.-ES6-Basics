4306a7f2ef0ac69b4c2a4a189845c865ee4301b9
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-09-19 14:11:20 +0300

    Fix test

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -12,6 +12,19 @@ app.get('/cart/:id(\\d+)', (req, res) => {
   res.status(200).send(`Payment methods for cart ${id}`);
 });
 
+app.get('/available_payments', (_req, res) => {
+  res.status(200).json({
+    payment_methods: {
+      credit_cards: true,
+      paypal: false,
+    },
+  });
+});
+
+app.post('/login', (req, res) => {
+  res.status(200).send(`Welcome ${req.body.userName}`);
+});
+
 app.listen(PORT, () => {
   console.log(`API available on localhost port ${PORT}`);
 });
